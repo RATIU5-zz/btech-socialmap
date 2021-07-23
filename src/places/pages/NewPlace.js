1fbae8706866381a2083ae8bda30379adb8b793c
unknown <jasonmemm@gmail.com>
2021-07-23 14:03:26 -0600

    Added basic input validation

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Input from "../../shared/components/FormElements/Input";
+import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
 
 import style from "./NewPlace.module.css";
 
 const NewPlace = () => {
+	const submitHandler = event => {
+		event.preventDefault();
+	};
+
 	return (
-		<form className={`${style["place-form"]}`}>
-			<Input type="text" label="Title" />
+		<form className={`${style["place-form"]}`} onSubmit={submitHandler}>
+			<Input
+				type="text"
+				label="Title"
+				errorText="Please enter a valid title"
+				validators={[VALIDATOR_REQUIRE()]}
+			/>
 		</form>
 	);
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,25 +1,74 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+import { validate } from "../../util/validators";
 
 import style from "./Input.module.css";
 
-const Input = props => {
-	const [inputValue, setInputValue] = useState(null);
+const inputReducer = (state, action) => {
+	switch (action.type) {
+		case "CHANGE":
+			return {
+				...state,
+				value: action.value,
+				isValid: validate(action.value, action.validators),
+			};
+		case "TOUCH":
+			return {
+				...state,
+				isTouched: true,
+			};
+		default:
+			return state;
+	}
+};
 
-	const element = props.type ? (
-		<input id={props.id} type={props.type} value={inputValue} placeholder={props.placeholder} />
-	) : (
-		<textarea
-			id={props.id}
-			rows={props.rows || 3}
-			value={inputValue}
-			placeholder={props.placeholder}
-		/>
-	);
+const Input = props => {
+	const [inputState, dispatch] = useReducer(inputReducer, {
+		value: "",
+		isValid: false,
+		isTouched: false,
+	});
+
+	const inputChangeHandler = event => {
+		dispatch({ type: "CHANGE", value: event.target.value, validators: props.validators });
+	};
+
+	const inputTouchHandler = () => {
+		dispatch({ type: "TOUCH" });
+	};
+
+	const element =
+		props.type && props.type !== "textarea" ? (
+			<input
+				id={props.id}
+				type={props.type}
+				value={inputState.value}
+				onChange={inputChangeHandler}
+				onBlur={inputTouchHandler}
+				placeholder={props.placeholder}
+			/>
+		) : (
+			<textarea
+				id={props.id}
+				rows={props.rows || 3}
+				value={inputState.value}
+				onChange={inputChangeHandler}
+				onBlur={inputTouchHandler}
+				placeholder={props.placeholder}
+			/>
+		);
 
 	return (
-		<div className={`${style["form-control"]}`}>
+		<div
+			className={`${style["form-control"]} ${
+				!inputState.isValid && inputState.isTouched && style["form-control--invalid"]
+			}`}
+		>
 			<label htmlFor={props.id}>{props.label}</label>
 			{element}
+			{!inputState.isValid && inputState.isTouched && (
+				<p>{props.errorText || "Input is not valid"}</p>
+			)}
 		</div>
 	);
 };
