cf57e10d271fb974091776b5027ea727efef4078
RATIU5 <jasonmemm@gmail.com>
2021-07-27 14:23:22 -0600

    Added relation between users and places

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -38,7 +38,7 @@ const signUserUp = async (req, res, next) => {
 		email,
 		password,
 		image: "https://images.unsplash.com/photo-1626639900810-cfebc6de0eae?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
-		places: 0,
+		places: [],
 	});
 
 	try {
